7313dcdab9252c40a0a6e992905d6146705905e6
Daniel Haanpaa <dhaanpaa@djhent.us>
2023-11-20 11:01:54 -0800

    Added UMS

---
+++ NexImagePanel.UMS/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Windows;
+
+namespace NexImagePanel.UMS
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Services = ConfigureServices();
+        }
+
+        public IServiceProvider Services { get; private set; }
+
+
+        /// <summary>
+        /// Configures the services for the application.
+        /// </summary>
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory()) // Set the base path where the configuration file is located
+                .AddJsonFile("appsettings.json"); // Add the JSON configuration file
+            services.AddSingleton<IConfiguration>(builder.Build());
+       
+            return services.BuildServiceProvider();
+        }
+    }
+}
+
+
