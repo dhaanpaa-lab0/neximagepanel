53f0546fd00b2049ac4744b7b52a75d2e7c9f1f8
Daniel Haanpaa <dhaanpaa@djhent.us>
2023-11-27 20:50:08 -0800

    Added local dist

--- NEXImageControlPanel/CreateDistribution.ps1
+++ NEXImageControlPanel/CreateDistribution.ps1
@@ -1,3 +1,25 @@
 #!/usr/bin/env pwsh
+# Output folder
+$outputFolderPath = "./out"
+$outputFolderExists = Test-Path $outputFolderPath -PathType Container
+if (!$outputFolderExists) {
+    New-Item -ItemType Directory -Path $outputFolderPath
+}
 
-dotnet publish -o ./dist
+# Dist folder
+$distFolderPath = "./dist"
+$distFolderExists = Test-Path $distFolderPath -PathType Container
+if (!$distFolderExists) {
+    New-Item -ItemType Directory -Path $distFolderPath
+}
+
+
+$dt = [datetime]::Now.ToString("MMddyy-hhmmsss")
+Write-Host $dt
+dotnet publish -o $distFolderPath
+
+$productVersionHash = (Get-Item .\dist\NEXImageControlPanel.exe).VersionInfo.ProductVersion.Split(".") | Select-Object -Last 1
+$productPackFile = "./out/NICPDist.$($productVersionHash).$dt.zip"
+Get-ChildItem -Path $distFolderPath -Recurse |
+  Compress-Archive -DestinationPath $productPackFile
+
